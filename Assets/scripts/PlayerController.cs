50951ac92ea467c4d7faa03558194d83751d13bf
THAWATCHAI JOYSARN <thawatchai.joys@bulive.net>
2016-01-27 11:58:59 +0700

    Creating shots

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,35 +7,34 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
-	private Rigidbody rb;
-	
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	public Rigidbody rb;
 	
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody> ();
+	void Start(){
+		
+		rb = GetComponent<Rigidbody> ();     //The new shape to push an information from Rigidbody. I think.
+		
 	}
 	
-	void FixedUpdate() 
+	void FixedUpdate ()
 	{
-		float mH = Input.GetAxis ("Horizontal");  
-		float mV = Input.GetAxis ("Vertical"); 
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 		
-		Vector3 movment = new Vector3 (mH, 0.0F, mV);
-		rb.velocity = movment * speed;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
 		
 		rb.position = new Vector3 
 			(
-				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
-				0.0F,
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
 				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
 				);
 		
-		rb.rotation = Quaternion.Euler (0.0F, 0.0f, rb.velocity.x * -tilt);
-	} 
-	
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
 }﻿
